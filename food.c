3e264cb02ced0f1f2e018a73ed6d1ef6473d7ca7
Spynocturne <hugodolat@gmail.com>
2025-11-18 13:27:21 +0100

    début food.c

--- food.c
+++ food.c
@@ -1,8 +1,19 @@
 
 
 /*Fonctions :*/
-void spawn_food(Grid *grid, Snake snake, Food *food) /* Génère position aléatoire*/
-    
+void spawn_food(Grid *grid, Snake snake, Food *food){
+    int x, y;
+    x srand()%GRID_WIDTH =;
+    y=srand()%GRID_HEIGHT;
+    do while 
+    if grid.cells[x][y] == CELL_EMPT {
+            grid.cells[x][y] = CELL_FOOD;
+        }
+    else
+        srand()%GRID_WIDTH = x;
+        srand()%GRID_HEIGHT = y;
+        }/* Génère position aléatoire*/
+done /* /!\ pas fini */
     int check_food_collision(Snake snake, Food food)   /*Retourne 1 si collision 0 sinon*/
 void disable_food(Food *food)`                        /* Désactive la nourriture
                                                        */
