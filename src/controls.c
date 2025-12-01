ec8d7cafcd629f7a60337b919fd047c9d86657e4
Spynocturne <hugodolat@gmail.com>
2025-12-01 15:52:22 +0100

    commentaire

--- src/controls.c
+++ src/controls.c
@@ -4,32 +4,32 @@ MLV_Keyboard_button get_key_pressed() {
     MLV_Keyboard_button touche;
     MLV_Button_state state;
 
-    MLV_get_event (
-        &touche, 
-        NULL, 
+    MLV_get_event (/*Si une touche est presser les infos que l'on prend sont :*/
+        &touche, /*La touche en question*/
+        NULL, /*les nulls servent a ne pas déclarer le reste de la fonction*/
         NULL,
         NULL,
         NULL,
         NULL,
         NULL,
         NULL,
-        &state
+        &state /*etat de la touche*/
     ); 
     
-    if (state == MLV_PRESSED) {
+    if (state == MLV_PRESSED) {  /*si une touche est presser, on retourne la touche en question */
         return touche;
     } else {
-        return MLV_KEYBOARD_NONE;
+        return MLV_KEYBOARD_NONE;/*rien n'est renvoyer car rien n'est pressé */
     }
 }
 
-ToucheClavier convert_key_to_enum(MLV_Keyboard_button key) {
+ToucheClavier convert_key_to_enum(MLV_Keyboard_button key) { /*Fonction qui permert d'attibuer une touche MLV a une touche du programme*/
     ToucheClavier touche = NONE;
 
-    switch (key) {
-        case MLV_KEYBOARD_UP:
-            touche = UP;
-            break;
+    switch (key) { /*switch est un if/else plus obtimiser quand on a plus de 2 élément*/
+        case MLV_KEYBOARD_UP:  /*case = if, la touche pressé est UP */
+            touche = UP; /*assigne à la varible touche: UP*/
+            break; /*fin du programme pour ne pas à avoir à tester les choses*/
         case MLV_KEYBOARD_DOWN:
             touche = DOWN;
             break;
@@ -53,11 +53,11 @@ ToucheClavier convert_key_to_enum(MLV_Keyboard_button key) {
 
 
 void clear_event() {
-    MLV_Keyboard_button key;
+    MLV_Keyboard_button key; /*variable "key" de type MLV_Keyboard_button*/
 
     while (
-        MLV_get_event (
-        &key, 
+        MLV_get_event ( /*les infos prises a chaque fois sont :*/
+        &key,  /*l'adresse par références de la varible key*/
         NULL, 
         NULL,
         NULL,
